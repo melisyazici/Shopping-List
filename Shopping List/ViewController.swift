1bf865c022cc6b994755199bc49b1b3e2ad43a39
Melis Yazıcı <melis.yazici95@icloud.com>
2022-10-22 03:27:21 +0200

    Initial Commit

---
+++ Shopping List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping List
+//
+//  Created by Melis Yazıcı on 22.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
