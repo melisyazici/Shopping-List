58fa932df7a8fb57e3c9cc75979dc4d390f57cc5
Melis Yazıcı <melis.yazici95@icloud.com>
2022-10-22 03:57:53 +0200

    Update Table View
    Add numberOfRowInSection and cellForRowAt methods

--- Shopping List/ViewController.swift
+++ Shopping List/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var shoppingList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
     }
 
 
