1df6763a021cd11afecfc758c9d19b24e2876038
Melis Yazıcı <melis.yazici95@icloud.com>
2022-10-22 04:23:32 +0200

    Create add button, addTapped and addItem method

--- Shopping List/ViewController.swift
+++ Shopping List/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -24,6 +26,23 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
         cell.textLabel?.text = shoppingList[indexPath.row]
         return cell
+    }
+    
+    @objc func addTapped() {
+        let ac = UIAlertController(title: "Add Item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let addAction = UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.addItem(item)
+        }
+        ac.addAction(addAction)
+        present(ac, animated: true)
+    }
+    
+    func addItem(_ item: String) {
+        shoppingList.insert(item, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic) // animate the new cell appearing
     }
 
 
